b659ed7993f4893c8726a1ebcaec3a16b66dba95
Arnel Reyes <founder@security-science.com>
2025-05-01 20:29:55 -0400

    v1.250430

---
+++ NMAP SSL Scanner/nmap_ssl_scanner.py
@@ -0,0 +1,188 @@
+# ---------------------------------------
+# Sec-Sci NMap SSL Scanner v1.250430 - April 2025
+# ---------------------------------------
+# Tool:      Sec-Sci AutoPT v1.250430
+# Site:      www.security-science.com
+# Email:     RnD@security-science.com
+# Creator:   ARNEL C. REYES
+# @license:  GNU GPL 3.0
+# @copyright (C) 2025 WWW.SECURITY-SCIENCE.COM
+
+from burp import IBurpExtender, IHttpListener, IScanIssue
+import subprocess
+import re
+
+hosts = []
+
+class BurpExtender(IBurpExtender, IHttpListener):
+
+    def registerExtenderCallbacks(self, callbacks):
+        self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
+        callbacks.setExtensionName("Sec-Sci NMap SSL Scanner")
+        callbacks.registerHttpListener(self)
+
+        print("[*] SSL Weak Cipher Scanner extension loaded.")
+
+
+    def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
+        # Only act on responses (not requests)
+        if messageIsRequest:
+            return None
+
+        httpService = messageInfo.getHttpService()
+        host = httpService.getHost()
+        port = httpService.getPort()
+        protocol = httpService.getProtocol()
+        request_url = self._helpers.analyzeRequest(messageInfo).getUrl()
+
+        # Skip if not https or the URL is out-of-scope
+        if protocol != "https" or not self._callbacks.isInScope(request_url):
+            return None
+
+        if host not in hosts:
+            hosts.append(host)
+            nmap_cmd = ["nmap", "--script", "ssl-enum-ciphers", "-p", str(port), host]
+            # print("[INFO] Running Nmap command: {}".format(nmap_cmd))
+
+            try:
+                nmap_output = subprocess.check_output(nmap_cmd, stderr=subprocess.STDOUT)
+                nmap_output = nmap_output.decode("utf-8")
+                # print("[DEBUG] Nmap output:\n" + nmap_output)
+            except subprocess.TimeoutExpired:
+                print("[ERROR] Nmap scan timed out")
+                return None
+            except Exception as e:
+                print("[ERROR] Nmap scan failed: {}".format(e))
+                return None
+
+            known_vulnerabilities = [
+                "Anonymous cipher suites supported",
+                "Low strength cipher suites supported",
+                "No support for TLSv1.2 or higher",
+                "Insecure renegotiation supported",
+                "Client-initiated renegotiation supported",
+                "Uses a block cipher in CBC mode with TLS 1.0 or earlier",
+                "Certificate uses weak signature algorithm",
+                "Key exchange (dh 1024) of lower strength than certificate key"
+                "Anonymous cipher suites supported (no authentication)",
+                "Low strength cipher suites supported",
+                "No support for TLSv1.2 or higher"
+            ]
+
+            # Weak Certificate
+            weak_cert_indicators = [
+                "Signature algorithm: md5WithRSAEncryption",   # MD5 signatures
+                "Signature algorithm: sha1WithRSAEncryption",  # SHA-1 deprecated
+                "Self-signed certificate",
+                "Certificate expired",
+                "Key size: 1024 bits",                         # Too small
+                "Public Key Algorithm: RSA (1024 bits)",
+                "Subject CN does not match domain"
+            ]
+
+            ssl_issues = [
+                ("SSLv2", "Deprecated protocol detected: SSLv2"),
+                ("SSLv3", "Deprecated protocol detected: SSLv23"),
+                ("TLSv1.0", "Deprecated protocol detected: TLSv1.0"),
+                ("TLSv1.1", "Deprecated protocol detected: TLSv1.1"),
+                ("SWEET32", "64-bit block cipher 3DES vulnerable to SWEET32 attack"),
+                ("lower strength", "Key exchange (dh 1024) of lower strength than certificate key"),
+                ("BEAST", "Vulnerable to BEAST attack"),
+                ("POODLE", "Vulnerable to POODLE attack"),
+                ("RC4", "Weak cipher suites detected: RC4"),  # RC4 is considered insecure / Broken stream cipher (insecure bias).
+                ("NULL", "Weak cipher suites detected: NULL"),  # No encryption at all
+                ("EXP", "Weak cipher suites detected: EXP"),  # Export-grade ciphers (e.g., 40/56-bit)
+                ("DES", "Weak cipher suites detected: DES"),  # Obsolete, easily broken
+                ("3DES", "Weak cipher suites detected: 3DES"),  # Vulnerable to SWEET32 (block size 64-bit)
+                ("MD5", "Weak cipher suites detected: MD5"),  # Weak hashing algorithm
+                ("SEED", "Weak cipher suites detected: SEED"),  # Not widely trusted; included for strictness
+                ("IDEA", "Weak cipher suites detected: IDEA"),  # Not widely trusted; included for strictness
+                ("CAMELLIA", "Weak cipher suites detected: CAMELLIA"),  # Not widely trusted; included for strictness
+                ("CBC", "Weak cipher suites detected: CBC"),  # Can be vulnerable to BEAST/Lucky13 depending on implementation
+                ("CRIME", "Weak cipher suites detected: CRIME")
+            ]
+
+            issues = []
+
+            for ssl_issue in ssl_issues:
+                if ssl_issue[0] in nmap_output:
+                    issues.append("- " + ssl_issue[1])
+
+            if issues:
+                issue_detail = """               
+                The server is configured to support weak SSL/TLS cipher suites, which could allow an attacker to decrypt 
+                or tamper with encrypted traffic through methods such as cryptographic downgrade attacks, brute force,
+                or protocol vulnerabilities.<br><br>
+                During SSL/TLS negotiation with the server, the following weak cipher suites were found to be supported:<br><br>
+                """ + "<br>".join(issues) + """<br><br>
+                Use of these cipher suites significantly reduces the strength of encryption and may expose sensitive
+                data to interception or modification. SSL Scanner initiated a TLS handshake and observed these weak
+                ciphers in the server's response. This indicates the server is not enforcing modern, secure cipher policies.
+                <br><br><pre>""" + nmap_output + """</pre><br>
+                <b>Issue background</b><br><br>
+                Cipher suites determine how TLS encryption is applied between the client and the server.
+                Older or weak cipher suites use outdated algorithms (e.g., RC4, 3DES, MD5, NULL) that are considered
+                insecure due to vulnerabilities or insufficient key lengths.<br><br>
+                Attackers may exploit these weak ciphers to:<br><br>
+                - Perform downgrade attacks (e.g., forcing use of export-grade or legacy ciphers)<br>
+                - Exploit specific vulnerabilities like SWEET32, FREAK, or LOGJAM<br>
+                - Break confidentiality or integrity of communications<br><br>
+                Modern TLS configurations should use only strong ciphers with forward secrecy and authenticated encryption,
+                such as those based on AES-GCM or ChaCha20-Poly1305.<br><br>
+                <b>Issue remediation</b><br><br>
+                Reconfigure the web server to:<br><br>
+                - Disable all weak, deprecated, or export-grade cipher suites<br>
+                - Enable only secure cipher suites that offer forward secrecy (e.g., ECDHE with AES-GCM)<br>
+                - Prefer TLS 1.2 and 1.3; disable SSL 2.0, SSL 3.0, TLS 1.0, and TLS 1.1<br><br>
+                Ensure the final configuration is tested using tools such as:<br><br>
+                - <a href="https://www.ssllabs.com/ssltest/">SSL Labs SSL Test</a><br>
+                - nmap --script ssl-enum-ciphers<br><br>
+                <b>References</b><br><br>
+                - <a href="https://cheatsheetseries.owasp.org/cheatsheets/Transport_Layer_Security_Cheat_Sheet.html">
+                OWASP: Transport Layer Protection Cheat Sheet</a><br>
+                - <a href="https://www.ssllabs.com/ssltest/">SSL Labs: SSL Server Test</a><br>
+                - <a href="https://ssl-config.mozilla.org/">Mozilla SSL Configuration Generator</a><br>
+                - <a href="https://datatracker.ietf.org/doc/html/rfc7525">RFC 7525: Recommendations for Secure Use of TLS</a><br><br>
+                <b>Vulnerability classifications</b><br><br>
+                - <a href="https://cwe.mitre.org/data/definitions/326.html">CWE-326: Inadequate Encryption Strength</a><br>
+                - <a href="https://cwe.mitre.org/data/definitions/327.html">CWE-327: Use of a Broken or Risky Cryptographic Algorithm</a><br>
+                - <a href="https://capec.mitre.org/data/definitions/242.html">CAPEC-242: Algorithm Downgrade Attack</a><br>
+                - <a href="https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-2183">CVE-2016-2183 (SWEET32)</a><br>
+                - <a href="https://www.first.org/cvss/calculator/3.1">CVSS v3.1 Calculator</a><br>
+                """
+
+                issue = SSLScanIssue(
+                    httpService,
+                    request_url,
+                    [messageInfo],
+                    "Weak TLS/SSL Configuration",
+                    issue_detail,
+                    "Medium"
+                )
+                self._callbacks.addScanIssue(issue)
+
+            # print("[INFO] No weak cipher support on %s:%s" % (host, port))
+
+
+class SSLScanIssue(IScanIssue):
+    def __init__(self, httpService, url, httpMessages, name, detail, severity):
+        self._httpService = httpService
+        self._url = url
+        self._httpMessages = httpMessages
+        self._name = name
+        self._detail = detail
+        self._severity = severity
+
+    def getUrl(self): return self._url
+    def getIssueName(self): return self._name
+    def getIssueType(self): return 0
+    def getSeverity(self): return self._severity
+    def getConfidence(self): return "Certain"
+    def getIssueBackground(self): return None
+    def getRemediationBackground(self): return None
+    def getIssueDetail(self): return self._detail
+    def getRemediationDetail(self): return None
+    def getHttpMessages(self): return self._httpMessages
+    def getHttpService(self): return self._httpService
+
